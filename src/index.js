07585e6296d498adee4537d1233de3188423bb2e
Samuel Lindqvist <samuel.lindqvist@noccela.fi>
2020-02-20 17:32:51 +0200

    Export default options

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // Expose all relevant public properties.
-export { EVENT_TYPES } from "./constants/constants";
+export { EVENT_TYPES, DEFAULT_OPTIONS } from "./constants/constants";
 export { getToken } from "./rest/authentication";
 export { EventChannel } from "./socket/eventhandler";
