27eff018c6066c9d82ec75d45fc24abc00c0ad92
Jukka-Pekka Tuomanen <jukka-pekka.tuomanen@noccela.fi>
2021-12-14 16:34:07 +0200

    remove debug paths

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -7,10 +7,6 @@ export const NCC_PATHS = {
     NODE_DOMAIN: "/realtime/domain"
 };
 
-//export const DEFAULT_API_WS_ORIGIN = "wss://api.noccela.com";
-//export const DEFAULT_API_HTTP_ORIGIN = "https://api.noccela.com";
-//export const DEFAULT_AUTH_ORIGIN = "https://auth.noccela.com";
-
-export const DEFAULT_API_WS_ORIGIN = "ws://api.jp.noccela.xyz";
-export const DEFAULT_API_HTTP_ORIGIN = "http://api.jp.noccela.xyz";
-export const DEFAULT_AUTH_ORIGIN = "http://auth.jp.noccela.xyz";
+export const DEFAULT_API_WS_ORIGIN = "wss://api.noccela.com";
+export const DEFAULT_API_HTTP_ORIGIN = "https://api.noccela.com";
+export const DEFAULT_AUTH_ORIGIN = "https://auth.noccela.com";
